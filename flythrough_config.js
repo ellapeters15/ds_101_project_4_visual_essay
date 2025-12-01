7e637c766f4b31bc7990de0e3ffddb3f51daf176
Ella Peters <pipapeters@gmail.com>
2025-12-01 13:21:54 -0500

    quad

--- flythrough_config.js
+++ flythrough_config.js
@@ -40,7 +40,7 @@ var config = {
             id: 'intro',
             title: 'Reddit Sentiment Analysis: Two College Towns based off ODU and JMU',
             description: 'Exploring how students discuss their campuses on Reddit - comparing JMU in Harrisonburg, VA and ODU in Norfolk, VA.',
-            image: './images/quad.jpg',
+            image: './images/jmu_quad.jpg',
             duration: 2500,
             
             // Camera position (where to fly)
@@ -250,7 +250,7 @@ var config = {
             id: 'conclusion',
             title: 'Conclusion: Comparing Campus Sentiment',
             description: 'Our analysis reveals both similarities and differences in how students experience and discuss these two universities.',
-            image: './images/quad.jpg',
+            image: './images/jmu_quad.jpg',
             duration: 2500,
             
             camera: {
